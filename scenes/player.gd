a2c112f1cc5396417671a162fd40a5ad2853dbbc
Shaun Hoerner <sh.hoerner@gmail.com>
2024-02-21 01:53:56 -0700

    Updated player movement
    acceleration kinda works now, but speed does not account for change in direction.

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,21 +1,20 @@
 extends CharacterBody2D
 
 @export var speed = 500
+@export var acceleration = 1
 @export var rotation_speed = PI+(PI*1/3)
-
-var rotation_direction = 0
-
-func get_input():
-	rotation_direction = Input.get_axis("ccw", "cw")
-	velocity = transform.x * Input.get_axis("back", "forward") * speed
+var linear_direction
+var rotation_direction
 
 func _physics_process(delta):
-	get_input()
+	get_direction()
 	
 	if Input.is_action_pressed("forward"):
 		$linear_animated.play("forward")
+		velocity = transform.y * move_toward(velocity.length(),speed,acceleration) * linear_direction
 	elif Input.is_action_pressed("back"):
 		$linear_animated.play("back")
+		velocity = transform.y * move_toward(velocity.length(),speed,acceleration) * linear_direction
 	elif Input.is_action_pressed("back") and Input.is_action_pressed("forward"):
 		$linear_animated.stop()
 	else:
@@ -23,12 +22,17 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ccw"):
 		$rotation_animated.play("ccw")
+		rotation += rotation_direction * rotation_speed * delta
 	elif Input.is_action_pressed("cw"):
 		$rotation_animated.play("cw")
+		rotation += rotation_direction * rotation_speed * delta
 	elif Input.is_action_pressed("ccw") and Input.is_action_pressed("cw"):
 		$rotation_animated.stop()
 	else:
 		$rotation_animated.stop()
 	
-	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
+
+func get_direction():
+	linear_direction = Input.get_axis("forward", "back")
+	rotation_direction = Input.get_axis("ccw","cw")
