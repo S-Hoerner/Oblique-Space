88906157009b7b6cb8d0cfae06a63cfa60cc762c
Shaun Hoerner <sh.hoerner@gmail.com>
2024-02-20 22:30:04 -0700

    Better animations for player

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-@export var speed = 400
-@export var rotation_speed = PI
+@export var speed = 500
+@export var rotation_speed = PI+(PI*1/3)
 
 var rotation_direction = 0
 
@@ -11,9 +11,24 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
+	
 	if Input.is_action_pressed("forward"):
-		$AnimatedSprite2D.play()
+		$linear_animated.play("forward")
+	elif Input.is_action_pressed("back"):
+		$linear_animated.play("back")
+	elif Input.is_action_pressed("back") and Input.is_action_pressed("forward"):
+		$linear_animated.stop()
 	else:
-		$AnimatedSprite2D.stop()
+		$linear_animated.stop()
+	
+	if Input.is_action_pressed("ccw"):
+		$rotation_animated.play("ccw")
+	elif Input.is_action_pressed("cw"):
+		$rotation_animated.play("cw")
+	elif Input.is_action_pressed("ccw") and Input.is_action_pressed("cw"):
+		$rotation_animated.stop()
+	else:
+		$rotation_animated.stop()
+	
 	rotation += rotation_direction * rotation_speed * delta
 	move_and_slide()
