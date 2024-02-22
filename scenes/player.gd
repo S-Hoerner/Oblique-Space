cb6a972a33d30ffceaad0bd320d34a59b28b27c7
Shaun Hoerner <sh.hoerner@gmail.com>
2024-02-21 22:52:49 -0700

    updated movement code
    Final? movement code. I was being really dumb

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
 
-@export var speed = 500
-@export var acceleration = 1
-@export var rotation_speed = PI+(PI*1/3)
+@export var speed = 100
+@export var acceleration = 0.075
+@export var rotation_speed = PI
 var linear_direction
 var rotation_direction
 
 func _physics_process(delta):
 	get_direction()
 	
+	animatate()
+	
+	velocity += transform.y * linear_direction * (acceleration * speed)
+	rotation += rotation_direction * rotation_speed * delta
+	
+	move_and_slide()
+
+func animatate():
 	if Input.is_action_pressed("forward"):
 		$linear_animated.play("forward")
-		velocity = transform.y * move_toward(velocity.length(),speed,acceleration) * linear_direction
 	elif Input.is_action_pressed("back"):
 		$linear_animated.play("back")
-		velocity = transform.y * move_toward(velocity.length(),speed,acceleration) * linear_direction
 	elif Input.is_action_pressed("back") and Input.is_action_pressed("forward"):
 		$linear_animated.stop()
 	else:
@@ -22,16 +28,12 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ccw"):
 		$rotation_animated.play("ccw")
-		rotation += rotation_direction * rotation_speed * delta
 	elif Input.is_action_pressed("cw"):
 		$rotation_animated.play("cw")
-		rotation += rotation_direction * rotation_speed * delta
 	elif Input.is_action_pressed("ccw") and Input.is_action_pressed("cw"):
 		$rotation_animated.stop()
 	else:
 		$rotation_animated.stop()
-	
-	move_and_slide()
 
 func get_direction():
 	linear_direction = Input.get_axis("forward", "back")
