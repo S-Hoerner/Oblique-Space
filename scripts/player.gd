369d9f3b5352d5fc09a7a536521793b382bd9874
Shaun Hoerner <sh.hoerner@gmail.com>
2024-02-24 12:37:23 -0700

    added basic health system
    when player collides with a "danger" player takes. However, what counts as a collision is as of yet unknown.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,36 +1,70 @@
 extends RigidBody2D
 
+signal died
+signal hit
 
 @export var speed = 500
 @export var acceleration = 0.075
 @export var rotation_speed = 1000
+@export var life = 10
+@export var invinc_time = 0.25
 var linear_direction
 var rotation_direction
 var applied_force
+var dead
+var invincible
+
 
 func _ready():
-	pass
+	player_not_dead()
+	$invincible_animated.hide()
 
 func _physics_process(delta):
-	get_direction()
-	
-	animatate()
-	
-	applied_force = Vector2(0,speed).rotated(rotation)*linear_direction
-	
-	if Input.is_action_pressed("slow"):
-		sleeping = false
-		apply_central_force(applied_force/4)
-		apply_torque(rotation_speed*rotation_direction/4)
-		pass
-	elif Input.is_action_pressed("stop"):
-		#set_linear_damp(1)
-		sleeping = true
-		pass
-	else:
-		sleeping = false
-		apply_central_force(applied_force)
-		apply_torque(rotation_speed*rotation_direction)
+	if dead == false:
+		get_direction()
+		
+		animatate()
+		
+		applied_force = Vector2(0,speed).rotated(rotation)*linear_direction
+		
+		if Input.is_action_pressed("slow"):
+			sleeping = false
+			apply_central_force(applied_force/4)
+			apply_torque(rotation_speed*rotation_direction/4)
+			pass
+		elif Input.is_action_pressed("stop"):
+			#set_linear_damp(1)
+			sleeping = true
+			pass
+		else:
+			sleeping = false
+			apply_central_force(applied_force)
+			apply_torque(rotation_speed*rotation_direction)
+
+func _on_body_entered(body):
+	if invincible == false:
+		if body.is_in_group("dangers"):
+			life -= 1
+			invincible = true
+			emit_signal("hit")
+		if life == 0:
+			player_dead()
+
+func player_dead():
+	dead = true
+	hide()
+	#freeze = true
+	sleeping = true
+	$CollisionPolygon2D.set_deferred("disabled",true)
+	emit_signal("died")
+
+func player_not_dead():
+	dead = false
+	freeze = false
+	sleeping = false
+	invincible = false
+	show()
+	$CollisionPolygon2D.set_deferred("disabled",false)
 
 func animatate():
 	if Input.is_action_pressed("forward"):
@@ -65,3 +99,12 @@ func animatate():
 func get_direction():
 	linear_direction = Input.get_axis("forward", "back")
 	rotation_direction = Input.get_axis("ccw","cw")
+
+func _on_hit():
+	if invincible == true:
+		$invincible_animated.show()
+		$invincible_animated.play("invincible")
+		await get_tree().create_timer(invinc_time).timeout
+		$invincible_animated.stop()
+		$invincible_animated.hide()
+		invincible = false
